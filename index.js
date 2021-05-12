e2bd369f9ee0941fb54ace6dde9a36c1173c6e78
Anri Kawahara <anri0806@hotmail.co.jp>
2021-05-12 03:14:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Add your doToElementsInArray() function here:
-doToElementsInArray(animals, changeCompletely);
- console.log(animals);
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
+
 
 // Add your changeCompletely() function here:
 function changeCompletely(element, index, array) {
